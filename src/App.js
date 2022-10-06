1b8312f590a38fe3b4bb330c2e5ba79ab53dbd9d
hoon2 <sanghoon.engineer@gmail.com>
2022-10-06 09:40:56 +0900

    07 시작 코드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>hello-block-chain-school</h1>
+      </header>
+      <div style={{ border: "1px solid black", width: "max-content" }}>
+        <div style={{ background: "red", width: 150, height: 200 }}></div>
+        <div>
+          <p>Name</p>
+          <p>Dog 44</p>
+        </div>
+        <div>
+          <p>Price</p>
+          <p>0.423 Either</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
